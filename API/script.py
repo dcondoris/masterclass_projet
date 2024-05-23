cccf28d310cc41825719777a7672a8846f3810df
dcondoris <dimitritis@hotmail.fr>
2024-05-23 16:14:01 +0200

    feat

--- API/script.py
+++ API/script.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/addition")
 def addition(a: int, b: int):
-    return a**3 + b**3
+    return a + b
+
+@add.get("/soustraction")
+def soustraction(a: int, b: int):
+    return a - b
